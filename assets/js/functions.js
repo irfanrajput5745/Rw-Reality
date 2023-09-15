c972b6adb118e85396dc2f6bfe991e216b5727a5
Muhammad Irfan <10283@contoso.local>
2023-09-16 04:51:52 +0500

    new project

---
+++ assets/js/functions.js
@@ -0,0 +1,144 @@
+// sticky header
+/*$(window).scroll(function() {    
+var scroll = $(window).scrollTop();
+//>=, not <=
+if (scroll >= 300) {
+//clearHeader, not clearheader - caps H
+$("header").addClass("stickyheader");
+} else {
+$("header").removeClass("stickyheader");  
+}
+});*/
+// sticky header end
+
+// wow start
+new WOW().init();
+// wow end
+
+// sticky social end
+
+$(document).ready(function() {
+"use strict";
+
+// Active Current Page
+var str=location.href.toLowerCase();
+$(".nav-area-full a").each(function() {
+if (str.indexOf(this.href.toLowerCase()) > -1) {
+$(".active").removeClass("active");
+$(this).parent().addClass("active");
+}
+});	
+// Active Current Page end
+    
+////// main slider
+ $(".feature_properties_slider").slick({
+    dots: true,
+    arrows: false,
+    infinite: !0,
+    speed: 1500,
+    slidesToShow: 4,
+    slidesToScroll:1,
+    autoplay: false,
+    autoplaySpeed: 4000,
+    adaptiveHeight: !0,
+    responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+    dots: true,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+    });
+
+////// testimonial slider
+$(".testimonials-slider-main").slick({
+  dots: true,
+  arrows: false,
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  speed: 1500,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  //fade: true,
+  //cssEase: 'linear',    
+    });  
+////// testimonial slider end
+  ////// mob slider
+    $(".sliderxs").slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+		adaptiveHeight: true,
+        responsive: [
+            {
+              breakpoint: 10000,
+              settings: "unslick"
+            },
+            {
+              breakpoint: 767,
+              settings: {
+                unslick: true
+              }
+            }
+        ]
+    });
+////// mob slider end
+
+
+
+
+
+
+////// tabs generic (nav and tabs in main div)
+$('.tab-custom .tab-custom-nav a').click(function(event){
+$(this).closest('li').siblings('li').children('a').removeClass('current');
+$(this).addClass('current');
+$(this).closest('.tab-custom').children('div.tab-content-panel:not(:hidden)').hide();
+$(this.hash).show();
+event.preventDefault();
+$('.sliderxs').slick('setPosition');
+});
+////// tabs generic end
+
+////// tabs custom (place nav and tabs anywhere separately)
+$('.tabs-custom-nav a').click(function(event){
+$(this).closest('li').siblings('li').children('a').removeClass('current');
+$(this).addClass('current');
+$(this.hash).closest('.general').children('div.tab-content-panel:not(:hidden)').hide();
+$(this.hash).show();
+event.preventDefault();
+$('.sliderxs').slick('setPosition');
+});
+////// tabs custom end
+
+////// Accordion 
+$('.accordion .quest-title.active1').addClass('active');
+$('#accordion-1').slideDown(300).addClass('open');
+function close_accordion_section() {
+jQuery('.accordion .quest-title').removeClass('active');
+jQuery('.accordion .quest-content').slideUp(300).removeClass('open');
+}
+jQuery('.quest-title').click(function(e) {
+// Grab current anchor value
+var currentAttrValue = jQuery(this).attr('href');
+if(jQuery(e.target).is('.active')) {
+close_accordion_section();
+}else {
+close_accordion_section();
+// Add active class to section title
+jQuery(this).addClass('active');
+// Open up the hidden content panel
+jQuery('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
+}
+e.preventDefault();
+});
+////// Accordion end 
+}); 
+// document ready end
+
+ 
